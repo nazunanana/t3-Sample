df7d36cfd6cd7bdd4ab9b7a90315b7612c9b3d10
nazunanana <azukiii1953@gmail.com>
2024-11-14 18:51:21 +0900

    '検索欄とチャット送信欄を作った'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
+import { IoSearchSharp, IoSendSharp } from "react-icons/io5";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
     // UIテスト用ページ
     return (
         <HydrateClient>
-                <div className="flex justify-center items-center h-screen">
-                    <form>
-                        <div>
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                ラベル+入力欄
-                            </label>
-                            <input className="appearance-none border rounded-md py-2 px-3 focus:outline-none" type="text" />
-                        </div>
-                    </form>
-                </div>
+            <div className="flex justify-center items-center h-screen bg-[#ffd1cc]">
+                <form>
+                    <label className="block text-gray-700 text-sm font-bold">
+                        入力欄
+                    </label>
+                    <input className="appearance-none border rounded-2xl py-1 px-3 mb-3 focus:outline-none" type="text" />
+
+                    <label className="block text-gray-700 text-sm font-bold">
+                        検索欄
+                    </label>
+                    <div className="flex items-center space-x-1 mb-3">
+                        <IoSearchSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                        <input className="appearance-none border rounded-2xl py-1 px-3 focus:outline-none" type="text" />
+                    </div>
+
+                    <label className="block text-gray-700 text-sm font-bold">
+                        チャット送信
+                    </label>
+                    <div className="flex items-center space-x-2 mb-3">
+                        <input className="appearance-none border rounded-2xl py-1 px-3 focus:outline-none" type="text" />
+                        <IoSendSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                    </div>
+                </form>
+            </div>
         </HydrateClient>
     );
 }
