dea38d1aa4fc6efe76ff12e4c6056df9b6f34d52
nazunanana <azukiii1953@gmail.com>
2024-11-17 00:58:24 +0900

    app.cssが適応される。labelのclassName定義

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,35 +18,41 @@ export default async function Home() {
 
     return (
         <HydrateClient>
-            <div>
-                <label className="block text-gray-700 text-sm font-bold">
-                    入力欄
-                </label>
-                <input className="appearance-none border rounded-2xl py-1 px-3 mb-3 focus:outline-none" type="text" />
+            <div className="layout">
+                <div>
+                    <label className="label">
+                        入力欄
+                    </label>
+                    <input className="appearance-none border rounded-2xl py-1 px-3 mb-3 focus:outline-none" type="text" />
 
-                <label className="block text-gray-700 text-sm font-bold">
-                    検索欄
-                </label>
-                <div className="flex items-center space-x-1 mb-3">
-                    <IoSearchSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
-                    <input className="appearance-none border rounded-2xl py-1 px-3 focus:outline-none" type="text" />
-                </div>
+                    <label className="label">
+                        検索欄
+                    </label>
+                    <div className="flex items-center space-x-1 mb-3">
+                        <IoSearchSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                        <input className="appearance-none border rounded-2xl py-1 px-3 focus:outline-none" type="text" />
+                    </div>
 
-                <label className="block text-gray-700 text-sm font-bold">
-                    チャット送信
-                </label>
-                <div className="flex space-x-2 mb-3 items-end">
-                    <textarea rows={4} cols={30} className={textareaClass} />
-                    <button>
-                        <Link href="tag.tsx">
-                            <IoSendSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
-                        </Link>
-                    </button>
+                    <label className="label">
+                        チャット送信
+                    </label>
+                    <div className="flex space-x-2 mb-3 items-end">
+                        <textarea rows={4} cols={30} className={textareaClass} />
+                        <button>
+                            <Link href="tag.tsx">
+                                <IoSendSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                            </Link>
+                        </button>
+                    </div>
+
+                    <label className="label">
+                        可変テキストエリア
+                    </label>
+                    <ResizeTextarea className={textareaClass} />
+
+                    {/* TODO: Tag.tsxへのボタン実装する
+                https://tailwindui.com/components/application-ui/elements/buttons */}
                 </div>
-                <label className="block text-gray-700 text-sm font-bold">
-                    可変テキストエリア
-                </label>
-                <ResizeTextarea className={textareaClass} />
             </div>
         </HydrateClient>
     );
